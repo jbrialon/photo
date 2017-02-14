88644b4af7f56ef072e14cc55d0e4a7cd8c305c5
Jérém <jeremzor@gmail.com>
2017-02-14 09:52:16 +0100

    feat: pushing the limits of webpack loaders

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -54,7 +54,7 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url-loader',
+        loader: 'image-size-loader',
         query: {
           limit: 10000,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
