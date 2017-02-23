a6c2fba1e71b2cd2dde92891f1354d482252d7fe
Jérém <jeremzor@gmail.com>
2017-02-23 10:25:02 +0100

    feat: dynamic content

---
+++ src/data/content.js
@@ -0,0 +1,43 @@
+export default {
+  meta: {
+    title: 'Complétement à l\'est //'
+  },
+  'albums': {
+    'ardeche': {
+      name: 'ardeche',
+      displayName: 'Ardèche',
+      cover: require('../assets/photos/ardeche/ardeche-14.jpg'),
+      text: 'parce que l\'Ardèche c\'est chouette'
+    },
+    'islande': {
+      name: 'islande',
+      displayName: 'Ísland',
+      cover: require('../assets/photos/islande/islande-53.jpg'),
+      text: '11 jours / 0 nuits pour faire le tour de l\'Islande en Clio 4x4'
+    },
+    'interlac': {
+      name: 'interlac',
+      displayName: 'Interlac',
+      cover: require('../assets/photos/interlac/interlac-25.jpg'),
+      text: '3 jours / 2 nuits pour relier le lac d\'Annecy au lac d\'Aix les bains à pied'
+    }
+  },
+  'menu': [
+    {
+      'title': 'instagram',
+      'link': 'https://www.instagram.com/jbrialon_/'
+    },
+    {
+      'title': 'vsco',
+      'link': 'http://vsco.co/jbrialon/'
+    },
+    {
+      'title': '500px',
+      'link': 'https://500px.com/jbrialon'
+    },
+    {
+      'title': 'vimeo',
+      'link': 'https://vimeo.com/jbrialon'
+    }
+  ]
+}
