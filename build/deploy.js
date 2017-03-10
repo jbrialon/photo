db2b89338716ebaa958364abdc08756e99d940dc
Jérém <jeremzor@gmail.com>
2017-03-10 14:19:57 +0100

    deploy redirect 🐛

--- build/deploy.js
+++ build/deploy.js
@@ -1,10 +1,5 @@
-var ghpages = require('gh-pages')
-var path = require('path')
 var fs = require('fs');
 
 fs.createReadStream('config/_redirects').pipe(fs.createWriteStream('dist/_redirects'));
 
-ghpages.publish(path.join(__dirname, '../dist'), function (err) {
-  if (err) console.error(err)
-  console.log('New deploy is live')
-})
+console.log('New deploy is live');
