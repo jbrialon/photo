cf612480439a31ee926d0f604da45980f1ca82b2
Jérémy Brialon <jeremy.brialon@isobar.com>
2020-01-14 10:54:42 +0100

    put pages into chunks

--- src/router/index.js
+++ src/router/index.js
@@ -1,46 +1,44 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Octnov from 'pages/Octnov'
-import Disclaimer from 'pages/Disclaimer'
-import Travels from 'pages/Travels'
-import Travel from 'pages/Travel'
-import About from 'pages/About'
-import MobileDetect from 'mobile-detect'
+// import Octnov from 'pages/Octnov'
+// import Disclaimer from 'pages/Disclaimer'
+// import Travels from 'pages/Travels'
+// import MobileDetect from 'mobile-detect'
 
-const md = new MobileDetect(window.navigator.userAgent)
-const isMobile = md.phone() !== null
+// const md = new MobileDetect(window.navigator.userAgent)
+// const isMobile = md.phone() !== null
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   linkExactActiveClass: 'active',
-  scrollBehavior (to, from, savedPosition) {
+  scrollBehavior () {
     return { x: 0, y: 0 }
   },
   routes: [
     {
       path: '/',
       name: 'Travels',
-      component: Travels,
+      component: () => import(/* webpackChunkName: "travels" */ 'pages/Travels.vue'),
       props: true
     },
     {
       path: '/octnov/',
       name: 'Octnov',
-      component: isMobile ? Disclaimer : Octnov,
+      components: () => import(/* webpackChunkName: "octnov" */ 'pages/Octnov.vue'),
       props: true
     },
     {
       path: '/album/:name',
       name: 'Travel',
-      component: Travel,
+      component: () => import(/* webpackChunkName: "travel" */ 'pages/Travel.vue'),
       props: true
     },
     {
       path: '/about',
       name: 'About',
-      component: About,
+      component: () => import(/* webpackChunkName: "about" */ 'pages/About.vue'),
       props: true
     }
   ]
