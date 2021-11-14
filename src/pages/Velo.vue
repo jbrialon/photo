25cbeb79d33bd28cd338fe57133ec3768bc90be1
Jérémy Brialon <jeremzor@gmail.com>
2021-11-14 19:08:10 +0100

    (velo): optimisation + photos example on trace

--- src/pages/Velo.vue
+++ src/pages/Velo.vue
@@ -3,15 +3,15 @@
     <div class='velo__content' v-cloak>
       <div class="title-wrap">
         <h1 class="title">Complètement à l'est //</h1>
-        <span class="subtitle">
+        <span class="subtitle" v-if="false">
           Jour {{ activity.dayNumber }} :
           {{ velo[activity.dayNumber - 1].startLocation }} -
           {{ velo[activity.dayNumber - 1].endLocation }}
         </span>
-        <div class="subtitle">
+        <div class="subtitle" v-if="false">
           Distance : <animated-number :value="activity.totalDistance" :duration="1000" :formatValue="formatToKm" />
         </div>
-        <div class="subtitle">
+        <div class="subtitle" v-if="false">
           Denivelé : <animated-number :value="activity.totalElevation" :duration="1000" :formatValue="formatToElevation" />
         </div>
       </div>
@@ -30,9 +30,36 @@ import data from '../data/velo.json'
 import gju from 'geojson-utils'
 import MobileDetect from 'mobile-detect'
 import AnimatedNumber from 'animated-number-vue'
+import { parse, format } from 'date-fns'
+import { enGB } from 'date-fns/locale'
+import { find } from 'lodash'
 
 const md = new MobileDetect(window.navigator.userAgent)
 const startPath = [6.236866, 46.198834]
+let map = null
+let emptyGeoJson = {
+  'type': 'FeatureCollection',
+  'features': [
+    {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'LineString',
+        'coordinates': [startPath]
+      }
+    }
+  ]
+}
+const mapOptions = {
+  token: 'pk.eyJ1IjoiamJyaWFsb24iLCJhIjoiZjJkNjkyNDNiMzU0YjAxY2FjNGZlMjU3MGFiYjYyZmQifQ.lwFTmFgGxSuvfoJdTcx7Jg',
+  style: 'mapbox://styles/jbrialon/cktviax1t2amj19qooc4d32qy/draft',
+  center: [6.621296291298819, 46.31963262434434],
+  zoom: 10
+}
+
+const photosOnMap = []
+function inRange (x, min, max) {
+  return ((x - min) * (x - max) <= 0)
+}
 
 export default {
   name: 'velo',
@@ -48,9 +75,7 @@ export default {
       isMobile: md.phone() !== null,
       links: content.social,
       content: content.velo,
-      map: null,
       animation: null,
-      startPath: startPath,
       animationParams: {
         speedFactor: 45,
         index: 0
@@ -89,41 +114,43 @@ export default {
         totalDistance: 0,
         totalElevation: 0
       },
-      velo: data.velo,
-      geojson: geojson,
-      emptyGeoJson: {
-        'type': 'FeatureCollection',
-        'features': [
-          {
-            'type': 'Feature',
-            'geometry': {
-              'type': 'LineString',
-              'coordinates': [startPath]
-            }
-          }
-        ]
-      },
-      mapOptions: {
-        token: 'pk.eyJ1IjoiamJyaWFsb24iLCJhIjoiZjJkNjkyNDNiMzU0YjAxY2FjNGZlMjU3MGFiYjYyZmQifQ.lwFTmFgGxSuvfoJdTcx7Jg',
-        style: 'mapbox://styles/jbrialon/cktviax1t2amj19qooc4d32qy/draft',
-        center: [6.621296291298819, 46.31963262434434],
-        zoom: 10
-      }
+      velo: data.velo
     }
   },
   components: {
     'c-header': Header,
     'animated-number': AnimatedNumber
   },
+  computed: {
+    photos () {
+      // create a new context to get all images in assets/photos
+      const req = require.context('../assets/photos/velo', true, /\.jpg$/)
+      const photos = req.keys()
+        // return an Array of require items
+        .map(item => req(item))
+        .map((item) => {
+          const parsedDate = parse(item.exif.CreateDate, 'y:MM:dd HH:mm:ss', new Date(), { locale: enGB })
+          item.date = {
+            parsedDate: parsedDate,
+            day: format(parsedDate, 'dd'),
+            hour: format(parsedDate, 'HH'),
+            minute: format(parsedDate, 'mm')
+          }
+          return item
+        })
+      return photos
+    }
+  },
   methods: {
     animateLine (timestamp) {
       let currentDate = null
-      let times = this.geojson.features[0].properties.coordinateProperties.times
+      let currentDay = null
+      let times = geojson.features[0].properties.coordinateProperties.times
       // stop if it finishes
-      if (this.animationParams.index >= this.geojson.features[0].geometry.coordinates.length) {
+      if (this.animationParams.index >= geojson.features[0].geometry.coordinates.length) {
         let lastPos = [
-          this.geojson.features[0].geometry.coordinates[this.geojson.features[0].geometry.coordinates.length - 1][0],
-          this.geojson.features[0].geometry.coordinates[this.geojson.features[0].geometry.coordinates.length - 1][1]
+          geojson.features[0].geometry.coordinates[geojson.features[0].geometry.coordinates.length - 1][0],
+          geojson.features[0].geometry.coordinates[geojson.features[0].geometry.coordinates.length - 1][1]
         ]
         this.drawCircle(lastPos, this.animationParams.index)
         this.activity.totalDistance += this.velo[this.activity.dayNumber - 1].distance
@@ -132,42 +159,66 @@ export default {
       } else {
         // get the next position
         let pos = [
-          this.geojson.features[0].geometry.coordinates[this.animationParams.index][0],
-          this.geojson.features[0].geometry.coordinates[this.animationParams.index][1]
+          geojson.features[0].geometry.coordinates[this.animationParams.index][0],
+          geojson.features[0].geometry.coordinates[this.animationParams.index][1]
         ]
         // append new coordinates to the lineString
-        this.emptyGeoJson.features[0].geometry.coordinates.push(pos)
+        emptyGeoJson.features[0].geometry.coordinates.push(pos)
         if (this.animationParams.index > 88029 && this.animationParams.index < 88704) {
           this.animationParams.index += 3
         } else {
           this.animationParams.index += this.animationParams.speedFactor
         }
-
         currentDate = new Date(times[this.animationParams.index])
-        if (this.activity.currentDay !== currentDate.getDate()) {
+        currentDay = currentDate.getDate()
+
+        const photo = find(this.photos, (photo) => {
+          return Number(photo.date.day) === currentDay && Number(photo.date.hour) === currentDate.getHours() && inRange(currentDate.getMinutes(), Number(photo.date.minute) - 9, Number(photo.date.minute) + 9)
+        })
+
+        if (photo) {
+          if (!photosOnMap.includes(photo.src)) {
+            photosOnMap.push(photo.src)
+            let marker = new mapboxgl.Marker()
+            let coord = new mapboxgl.LngLat(
+              geojson.features[0].geometry.coordinates[this.animationParams.index][0],
+              geojson.features[0].geometry.coordinates[this.animationParams.index][1]
+            )
+            marker.setLngLat(coord)
+            marker.setPopup(
+              new mapboxgl.Popup({ offset: 25 }) // add popups
+                .setHTML(
+                  `<img src="${photo.src}" width="190px">`
+                )
+            )
+            marker.addTo(map)
+          }
+        }
+
+        if (this.activity.currentDay !== currentDay) {
           this.activity.currentDate = currentDate
-          this.activity.currentDay = currentDate.getDate()
+          this.activity.currentDay = currentDay
           this.activity.dayNumber++
           this.drawCircle(pos, this.animationParams.index)
-          this.map.fitBounds(this.activity.bounds[this.activity.dayNumber - 1], {
+          map.fitBounds(this.activity.bounds[this.activity.dayNumber - 1], {
             padding: this.isMobile ? 45 : 150
           })
           this.activity.totalDistance += this.velo[this.activity.dayNumber - 1].distance
           this.activity.totalElevation += this.velo[this.activity.dayNumber - 1].elevation
         }
         // then update the map
-        this.map.getSource('line').setData(this.emptyGeoJson)
+        map.getSource('line').setData(emptyGeoJson)
         // Request the next frame of the animation.
         this.animation = requestAnimationFrame(this.animateLine)
       }
     },
     mapload () {
-      this.map.addSource('line', {
+      map.addSource('line', {
         'type': 'geojson',
-        'data': this.emptyGeoJson
+        'data': emptyGeoJson
       })
 
-      this.map.addLayer({
+      map.addLayer({
         'id': 'line-animation',
         'type': 'line',
         'source': 'line',
@@ -182,15 +233,15 @@ export default {
         }
       })
 
-      this.map.fitBounds(this.activity.bounds[this.activity.dayNumber - 1], {
+      map.fitBounds(this.activity.bounds[this.activity.dayNumber - 1], {
         padding: this.isMobile ? 60 : 300
       })
       this.activity.totalDistance += this.velo[this.activity.dayNumber - 1].distance
       this.activity.totalElevation += this.velo[this.activity.dayNumber - 1].elevation
 
-      this.map.on('click', 'line-animation', (e) => {
-        var coordinates = this.geojson.features[0].geometry.coordinates
-        var times = this.geojson.features[0].properties.coordinateProperties.times
+      map.on('click', 'line-animation', (e) => {
+        var coordinates = geojson.features[0].geometry.coordinates
+        var times = geojson.features[0].properties.coordinateProperties.times
         var closestIndex = null
         var testDistance = null
         coordinates.forEach((coordinate, index) => {
@@ -210,13 +261,13 @@ export default {
       })
 
       // Mouse over gpx
-      this.map.on('mouseenter', 'line-animation', () => {
-        this.map.getCanvas().style.cursor = 'pointer'
+      map.on('mouseenter', 'line-animation', () => {
+        map.getCanvas().style.cursor = 'pointer'
       })
 
       // Mouse out GPX
-      this.map.on('mouseleave', 'line-animation', () => {
-        this.map.getCanvas().style.cursor = ''
+      map.on('mouseleave', 'line-animation', () => {
+        map.getCanvas().style.cursor = ''
       })
       // this.map.scrollZoom.disable()
 
@@ -229,7 +280,7 @@ export default {
       const sourceName = `source-circle-${index}`
       const id = `circle-${index}`
       let radius = 0
-      this.map.addSource(sourceName, {
+      map.addSource(sourceName, {
         'type': 'geojson',
         'data': {
           'type': 'FeatureCollection',
@@ -243,7 +294,7 @@ export default {
         }
       })
 
-      this.map.addLayer({
+      map.addLayer({
         'id': id,
         'type': 'circle',
         'source': sourceName,
@@ -260,7 +311,7 @@ export default {
         }
       })
 
-      this.map.addLayer({
+      map.addLayer({
         'id': `${id}_border`,
         'type': 'circle',
         'source': sourceName,
@@ -278,22 +329,22 @@ export default {
       })
 
       const anim = setInterval(() => {
-        this.map.setPaintProperty(id, 'circle-radius', radius)
+        map.setPaintProperty(id, 'circle-radius', radius)
         radius = ++radius % 20
         if (radius >= 10) {
           clearInterval(anim)
-          this.map.setPaintProperty(`${id}_border`, 'circle-stroke-opacity', 1)
+          map.setPaintProperty(`${id}_border`, 'circle-stroke-opacity', 1)
         }
       }, 50)
 
       // Mouse over circle
-      this.map.on('mouseenter', id, () => {
-        this.map.getCanvas().style.cursor = 'pointer'
+      map.on('mouseenter', id, () => {
+        map.getCanvas().style.cursor = 'pointer'
       })
 
       // Mouse out circle
-      this.map.on('mouseleave', id, () => {
-        this.map.getCanvas().style.cursor = ''
+      map.on('mouseleave', id, () => {
+        map.getCanvas().style.cursor = ''
       })
     },
     formatToKm (value) {
@@ -304,14 +355,14 @@ export default {
     }
   },
   mounted () {
-    mapboxgl.accessToken = this.mapOptions.token
-    this.map = new mapboxgl.Map({
+    mapboxgl.accessToken = mapOptions.token
+    map = new mapboxgl.Map({
       container: 'map', // container id
-      style: this.mapOptions.style, // stylesheet location
-      center: this.mapOptions.center, // starting position [lng, lat]
-      zoom: this.mapOptions.zoom // starting zoom
+      style: mapOptions.style, // stylesheet location
+      center: mapOptions.center, // starting position [lng, lat]
+      zoom: mapOptions.zoom // starting zoom
     })
-    this.map.on('load', this.mapload)
+    map.on('load', this.mapload)
   }
 }
 </script>
